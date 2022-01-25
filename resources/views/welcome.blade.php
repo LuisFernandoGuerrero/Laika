a45d8269a850ec14a034614668e892ff85139a4a
Luis Fernando Guerrero <fernandoguerrero.free@gmail.com>
2022-01-24 19:36:15 -0500

    main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAIKA</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/app.js') }}"></script>
 
-    </head>
-    
-    <body>
-        
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LAIKA</title>
+    <!-- Fonts -->
+    <link href="http://fonts.cdnfonts.com/css/armarialbold" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</head>
+
+<body>
+    <header class="header">
+        <div class="contenedor">
+            <div class="hamburguesa">
+                <div class="lineaCentral lineas"></div>
+            </div>
+            <div class="imagen">
+                <img src="{{ asset('img/LaikaMascotas.svg')}} " alt="laikaMascotas">
+                <h1 class="nombre">LAIKA</h1>
+            </div>
+            <div class="ciudad">
+                <img src="{{ asset('img/shopping-cart-solid.svg')}} " alt="laikaMascotas">
+            </div>
+            <div class="carrito">
+                <img src="{{ asset('img/shopping-cart-solid.svg')}} " alt="laikaMascotas">
+            </div>
+            <input type="text" placeholder="¿Qué necesita tu mascota?">
+        </div>
+    </header>
+</body>
+
 </html>
